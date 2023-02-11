98eca6178464d62d937cd2ec7e8cd33c37d3d406
Jimena Barseghian <aghassi.lucas@gmail.com>
2023-02-11 15:21:38 -0300

    Added Hellos func

--- greetings.go
+++ greetings.go
@@ -23,6 +23,25 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
+
 // init sets initial values for variables used in the function.
 func init() {
 	rand.Seed(time.Now().UnixNano())
@@ -55,4 +74,17 @@ This tells Go that the size of the array underlying the slice can be dynamically
 For more about init functions, see Effective Go.
 - In Hello, call the randomFormat function to get a format for the message you'll return, then use the format and name value together to create the message.
 - Return the message (or an error) as you did before.
+*/
+/*
+In this code, you:
+
+- Add a Hellos function whose parameter is a slice of names rather than a single name. Also, you change one of its return types from a string
+to a map so you can return names mapped to greeting messages.
+- Have the new Hellos function call the existing Hello function. This helps reduce duplication while also leaving both functions in place.
+- Create a messages map to associate each of the received names (as a key) with a generated message (as a value).
+In Go, you initialize a map with the following syntax: make(map[key-type]value-type). You have the Hellos function return this map to the caller.
+For more about maps, see Go maps in action on the Go blog.
+- Loop through the names your function received, checking that each has a non-empty value, then associate a message with each.
+In this for loop, range returns two values: the index of the current item in the loop and a copy of the item's value.
+You don't need the index, so you use the Go blank identifier (an underscore) to ignore it. For more, see The blank identifier in Effective Go.
 */
